3dff2a4de12e3f3180e55436de09076ab3c3e29c
Amy Ousterhout <aousterh@mit.edu>
2014-07-02 12:22:19 -0400

    rename topology to config

--- config.h
+++ config.h
@@ -1,16 +1,16 @@
 /*
- * topology.h
+ * config.h
  *
  *  Created on: June 23, 2014
  *      Author: aousterh
  */
 
-#ifndef TOPOLOGY_H_
-#define TOPOLOGY_H_
+#ifndef CONFIG_H_
+#define CONFIG_H_
 
 #define EMU_ROUTER_MAX_ENDPOINT_PORTS 32
 #define EMU_ROUTER_MAX_ROUTER_PORTS 0
 #define EMU_NUM_ROUTERS 1
 #define EMU_NUM_ENDPOINTS (EMU_ROUTER_MAX_ENDPOINT_PORTS * EMU_NUM_ROUTERS)
 
-#endif /* TOPOLOGY_H_ */
+#endif /* CONFIG_H_ */

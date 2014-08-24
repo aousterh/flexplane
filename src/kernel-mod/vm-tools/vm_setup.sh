96af21c28fac5689429065fa4c7df3e837d65aaf
Amy Ousterhout <aousterh@mit.edu>
2014-08-24 17:08:04 -0400

    add a script to set up an endpoint in a vm

---
+++ src/kernel-mod/vm-tools/vm_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# script to be run in a vm to setup a Fastpass endpoint
+
+# disable DHCP
+echo "disabling DHCP"
+pkill -f dhclient
+
+# configure an IP on the interface
+echo "configuring an IP for eth0"
+ifconfig eth0 down
+ifconfig eth0 up 10.1.1.1
+ifconfig eth0
+
+# synchronize time
+echo "synchronizing time"
+service ntp stop
+ntpdate -d 10.1.1.3
+
+# clear logs
+echo "clearing logs"
+./clear-logs.sh

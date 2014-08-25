7de589afccc5800b8f05b3eea37ed5f88bbcdbb0
Amy Ousterhout <aousterh@mit.edu>
2014-08-25 15:51:02 -0400

    fix some params in fp_tc.sh

--- src/kernel-mod/vm-tools/fp_tc.sh
+++ src/kernel-mod/vm-tools/fp_tc.sh
@@ -10,7 +10,7 @@ TC="/sbin/tc"
 
 ./del_tc.sh
 
-insmod fastpass.ko fastpass_debug=1 req_cost=1000000000 req_bucketlen=1000000000 ctrl_addr=$CONTROLLER_IP update_timer_ns=2048000 retrans_timeout_ns=3000000
+insmod fastpass.ko fastpass_debug=1 req_cost=1000000 req_bucketlen=1000000 ctrl_addr=$CONTROLLER_IP update_timer_ns=2048000 retrans_timeout_ns=3000000
 
 echo -- lsmod after --
 lsmod | grep fastpass
@@ -21,4 +21,4 @@ DATA=`cat /sys/module/fastpass/sections/.data`
 BSS=`cat /sys/module/fastpass/sections/.bss`
 echo add-symbol-file $FASTPASS_DIR/src/kernel-mod/fastpass.ko $TEXT -s .data $DATA -s .bss $BSS
 
-sudo $TC qdisc add dev $DEV root fastpass  req_cost 2000000 req_bucket 6000000 ctrl $CONTROLLER_IP rate 12500Kbps
+sudo $TC qdisc add dev $DEV root fastpass ctrl $CONTROLLER_IP rate 12500Kbps

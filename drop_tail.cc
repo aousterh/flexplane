b833220ae971256cc679af89f0d4f522b928d6d1
Jonathan Perry <yonch@yonch.com>
2014-12-15 23:01:04 -0500

    ported implementation of drop_tail

--- drop_tail.cc
+++ drop_tail.cc
@@ -10,20 +10,40 @@
 #include "api_impl.h"
 
 #define DROP_TAIL_PORT_CAPACITY 128
+#define DROP_TAIL_MAX_CAPACITY 256
+
+inline uint32_t DropTailClassifier::classify(struct emu_packet *pkt) {
+	return pkt->dst;
+}
+
+DropTailQueueManager::DropTailQueueManager(PacketQueueBank *bank,
+		uint32_t queue_capacity)
+	: m_bank(bank), m_q_capacity(queue_capacity)
+{
+	if (bank == NULL)
+		throw std::runtime_error("bank should be non-NULL");
+}
+
+inline void DropTailQueueManager::enqueue(struct emu_packet *pkt,
+		uint32_t queue_index)
+{
+	if (m_bank->occupancy(queue_index) >= m_q_capacity) {
+		/* no space to enqueue, drop this packet */
+		adm_log_emu_router_dropped_packet(&g_state->stat);
+		drop_packet(pkt);
+	} else {
+		m_bank->enqueue(queue_index, pkt);
+	}
+}
 
 DropTailRouter::DropTailRouter(uint16_t id, struct fp_ring *q_ingress,
-		struct drop_tail_args *args) : Router(id, q_ingress) {
-	uint32_t i, port_capacity;
-
-	/* use args if supplied, otherwise use defaults */
-	if (args != NULL) {
-		port_capacity = args->port_capacity;
-	} else
-		port_capacity = DROP_TAIL_PORT_CAPACITY;
-
-	for (i = 0; i < EMU_ROUTER_NUM_PORTS; i++)
-		queue_create(&this->output_queue[i], port_capacity);
-}
+		struct drop_tail_args *args)
+	: m_bank(EMU_ROUTER_NUM_PORTS, 1, DROP_TAIL_MAX_CAPACITY),
+	  m_cla(),
+	  m_qm(&m_bank, ((args == NULL) ? DROP_TAIL_PORT_CAPACITY : args->port_capacity)),
+	  m_sch(&m_bank),
+	  DropTailRouterBase(&m_cla, &m_qm, &m_sch, id, q_ingress)
+{}
 
 DropTailRouter::~DropTailRouter() {
 	uint16_t i;
@@ -31,32 +51,10 @@ DropTailRouter::~DropTailRouter() {
 
 	/* free all queued packets */
 	for (i = 0; i < EMU_ROUTER_NUM_PORTS; i++) {
-		while (queue_dequeue(&this->output_queue[i], &packet) == 0)
-			free_packet(packet);
+		while (!m_bank.empty(i))
+			free_packet(m_bank.dequeue(i));
 	}
 }
-
-void DropTailRouter::push(struct emu_packet *p) {
-	uint16_t output;
-	struct packet_queue *output_q;
-
-	output = get_output_queue(this, p);
-	output_q = &this->output_queue[output];
-
-	/* try to enqueue the packet */
-	if (queue_enqueue(output_q, p) != 0) {
-		/* no space to enqueue, drop this packet */
-		adm_log_emu_router_dropped_packet(&g_state->stat);
-		drop_packet(p);
-	}
-}
-
-void DropTailRouter::pull(uint16_t output, struct emu_packet **packet) {
-	/* dequeue one packet for this output if the queue is non-empty */
-	*packet = NULL;
-	queue_dequeue(&this->output_queue[output], packet);
-}
-
 
 DropTailEndpoint::DropTailEndpoint(uint16_t id, struct drop_tail_args *args) :
 		Endpoint(id) {
--- drop_tail.h
+++ drop_tail.h
@@ -14,6 +14,10 @@
 #include "router.h"
 #include "endpoint.h"
 #include "endpoint_group.h"
+#include "composite.h"
+#include "queue_bank.h"
+
+#include <stdexcept>
 
 struct packet_queue;
 
@@ -21,19 +25,60 @@ struct drop_tail_args {
 	uint16_t port_capacity;
 };
 
+
+class DropTailClassifier : public Classifier {
+public:
+	inline uint32_t classify(struct emu_packet *pkt);
+};
+
+class DropTailQueueManager : public QueueManager {
+public:
+	DropTailQueueManager(PacketQueueBank *bank, uint32_t queue_capacity);
+	inline void enqueue(struct emu_packet *pkt, uint32_t queue_index);
+
+private:
+	/** the QueueBank where packets are stored */
+	PacketQueueBank *m_bank;
+
+	/** the maximum capacity of each single queue */
+	uint32_t m_q_capacity;
+};
+
+
+class DropTailScheduler : public Scheduler {
+public:
+	DropTailScheduler(PacketQueueBank *bank) : m_bank(bank) {}
+
+	inline struct emu_packet *schedule(uint32_t output_port) {
+		if (m_bank->empty(output_port))
+			return NULL;
+		else
+			return m_bank->dequeue(output_port);
+	}
+
+private:
+	/** the QueueBank where packets are stored */
+	PacketQueueBank *m_bank;
+};
+
+typedef CompositeRouter<DropTailClassifier, DropTailQueueManager, DropTailScheduler>
+	DropTailRouterBase;
+
 /**
  * A drop tail router.
  * @output_queue: a queue of packets for each output port
  */
-class DropTailRouter : public Router {
+class DropTailRouter : public DropTailRouterBase {
 public:
 	DropTailRouter(uint16_t id, struct fp_ring *q_ingress,
 			struct drop_tail_args *args);
-	~DropTailRouter();
-	virtual void push(struct emu_packet *packet);
-	virtual void pull(uint16_t output, struct emu_packet **packet);
+	virtual ~DropTailRouter();
+
 private:
-	struct packet_queue output_queue[EMU_ROUTER_NUM_PORTS];
+	PacketQueueBank m_bank;
+	DropTailClassifier m_cla;
+	DropTailQueueManager m_qm;
+	DropTailScheduler m_sch;
 };
 
 /**

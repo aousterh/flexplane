1b440453a9f5ec863e751dc77041bc9f52fb5902
Jonathan Perry <yonch@yonch.com>
2014-12-19 18:12:33 -0500

    added PyQueueManager

---
+++ queue_managers/PyQueueManager.h
@@ -0,0 +1,21 @@
+/*
+ * PyQueueManager.h
+ *
+ *  Created on: Dec 19, 2014
+ *      Author: yonch
+ */
+
+#ifndef QUEUE_MANAGERS_PYQUEUEMANAGER_H_
+#define QUEUE_MANAGERS_PYQUEUEMANAGER_H_
+
+#include "../composite.h"
+
+class PyQueueManager: public QueueManager {
+public:
+	PyQueueManager() {};
+	virtual ~PyQueueManager() {};
+
+	virtual void enqueue(struct emu_packet *pkt, uint32_t port, uint32_t queue) {}
+};
+
+#endif /* QUEUE_MANAGERS_PYQUEUEMANAGER_H_ */

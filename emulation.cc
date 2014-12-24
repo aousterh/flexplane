c323a9d7e5deccc059d0268c18316e88e131df79
Hari Balakrishnan <hari@csail.mit.edu>
2014-12-24 15:59:43 -0500

    Trying to get RED etc to work with the changing TestEmulation framework

--- emulation.cc
+++ emulation.cc
@@ -24,165 +24,164 @@
 emu_state *g_state; /* global emulation state */
 
 static inline void free_packet_ring(struct emu_state *state,
-                                    struct fp_ring *packet_ring);
+		struct fp_ring *packet_ring);
 
 void emu_init_state(struct emu_state *state,
-                    struct fp_mempool *admitted_traffic_mempool,
-                    struct fp_ring *q_admitted_out, struct fp_mempool *packet_mempool,
-                    struct fp_ring **packet_queues, RouterType r_type, void *r_args,
-                    EndpointType e_type, void *e_args) {
-    uint32_t i, pq;
-    uint32_t size;
+		struct fp_mempool *admitted_traffic_mempool,
+		struct fp_ring *q_admitted_out, struct fp_mempool *packet_mempool,
+	    struct fp_ring **packet_queues, RouterType r_type, void *r_args,
+		EndpointType e_type, void *e_args) {
+	uint32_t i, pq;
+	uint32_t size;
 
-    g_state = state;
-    
-    pq = 0;
-    state->admitted_traffic_mempool = admitted_traffic_mempool;
-    state->q_admitted_out = q_admitted_out;
-    state->packet_mempool = packet_mempool;
-    
-    state->out = new EmulationOutput(state->q_admitted_out,
-                                     state->admitted_traffic_mempool, state->packet_mempool,
-                                     &state->stat);
+	g_state = state;
 
-    /* construct topology: 1 router with 1 rack of endpoints */
-    
-    /* initialize rings */
-    for (i = 0; i < EMU_NUM_ROUTERS; i++) {
-        state->q_router_ingress[i] = packet_queues[pq++];
-    }
-    state->q_epg_new_pkts[0] = packet_queues[pq++];
-    state->q_epg_ingress[0] = packet_queues[pq++];
+	pq = 0;
+	state->admitted_traffic_mempool = admitted_traffic_mempool;
+	state->q_admitted_out = q_admitted_out;
+	state->packet_mempool = packet_mempool;
 
-    Dropper dropper(*state->out);
-    
-    /* initialize all the routers */
-    for (i = 0; i < EMU_NUM_ROUTERS; i++) {
-        // TODO: use fp_malloc?
-        state->routers[i] = RouterFactory::NewRouter(r_type, r_args, i, dropper);
-        assert(state->routers[i] != NULL);
-        state->router_drivers[i] = new RouterDriver(state->routers[i],
-                                                    state->q_router_ingress[0], state->q_epg_ingress[0],
-                                                    &state->stat);
-    }
-    
-    /* initialize all the endpoints in one endpoint group */
-    state->endpoint_groups[0] = EndpointGroupFactory::NewEndpointGroup(e_type,
-                                                                       EMU_NUM_ENDPOINTS, *state->out);
-    state->endpoint_groups[0]->init(0, (struct drop_tail_args *) e_args);
-    assert(state->endpoint_groups[0] != NULL);
-    state->endpoint_drivers[0] =
-        new EndpointDriver(state->q_epg_new_pkts[0],
-                           state->q_router_ingress[0],
-                           state->q_epg_ingress[0],
-                           state->endpoint_groups[0],
-                           &state->stat);
+	state->out = new EmulationOutput(state->q_admitted_out,
+			state->admitted_traffic_mempool, state->packet_mempool,
+			&state->stat);
+
+	/* construct topology: 1 router with 1 rack of endpoints */
+
+	/* initialize rings */
+	for (i = 0; i < EMU_NUM_ROUTERS; i++) {
+		state->q_router_ingress[i] = packet_queues[pq++];
+	}
+	state->q_epg_new_pkts[0] = packet_queues[pq++];
+	state->q_epg_ingress[0] = packet_queues[pq++];
+
+	Dropper dropper(*state->out);
+
+	/* initialize all the routers */
+	for (i = 0; i < EMU_NUM_ROUTERS; i++) {
+		// TODO: use fp_malloc?
+		state->routers[i] = RouterFactory::NewRouter(r_type, r_args, i, dropper);
+		assert(state->routers[i] != NULL);
+		state->router_drivers[i] = new RouterDriver(state->routers[i],
+				state->q_router_ingress[0], state->q_epg_ingress[0],
+				&state->stat);
+	}
+
+	/* initialize all the endpoints in one endpoint group */
+	state->endpoint_groups[0] = EndpointGroupFactory::NewEndpointGroup(e_type,
+			EMU_NUM_ENDPOINTS, *state->out, 0, e_args);
+	assert(state->endpoint_groups[0] != NULL);
+	state->endpoint_drivers[0] =
+			new EndpointDriver(state->q_epg_new_pkts[0],
+					state->q_router_ingress[0],
+					state->q_epg_ingress[0],
+					state->endpoint_groups[0],
+					&state->stat);
 }
 
 void emu_cleanup(struct emu_state *state) {
-    uint32_t i;
-    struct emu_admitted_traffic *admitted;
-        
-    /* free all endpoints */
-    for (i = 0; i < EMU_NUM_ENDPOINT_GROUPS; i++) {
-        delete state->endpoint_drivers[i];
-        delete state->endpoint_groups[i];
-        
-        /* free packet queues, return packets to mempool */
-        free_packet_ring(state, state->q_epg_new_pkts[i]);
-        free_packet_ring(state, state->q_epg_ingress[i]);
-    }
-    
-    /* free all routers */
-    for (i = 0; i < EMU_NUM_ROUTERS; i++) {
-        delete state->router_drivers[i];
-        // TODO: call fp_free?
-        delete state->routers[i];
-        
-        /* free ingress queue for this router, return packets to mempool */
-        free_packet_ring(state, state->q_router_ingress[i]);
-    }
-    
-    delete state->out;
-    
-    /* empty queue of admitted traffic, return structs to the mempool */
-    while (fp_ring_dequeue(state->q_admitted_out, (void **) &admitted) == 0)
-        fp_mempool_put(state->admitted_traffic_mempool, admitted);
-    fp_free(state->q_admitted_out);
-    
-    fp_free(state->admitted_traffic_mempool);
-    fp_free(state->packet_mempool);
+	uint32_t i;
+	struct emu_admitted_traffic *admitted;
+
+	/* free all endpoints */
+	for (i = 0; i < EMU_NUM_ENDPOINT_GROUPS; i++) {
+		delete state->endpoint_drivers[i];
+		delete state->endpoint_groups[i];
+
+		/* free packet queues, return packets to mempool */
+		free_packet_ring(state, state->q_epg_new_pkts[i]);
+		free_packet_ring(state, state->q_epg_ingress[i]);
+	}
+
+	/* free all routers */
+	for (i = 0; i < EMU_NUM_ROUTERS; i++) {
+		delete state->router_drivers[i];
+		// TODO: call fp_free?
+		delete state->routers[i];
+
+		/* free ingress queue for this router, return packets to mempool */
+		free_packet_ring(state, state->q_router_ingress[i]);
+	}
+
+	delete state->out;
+
+	/* empty queue of admitted traffic, return structs to the mempool */
+	while (fp_ring_dequeue(state->q_admitted_out, (void **) &admitted) == 0)
+		fp_mempool_put(state->admitted_traffic_mempool, admitted);
+	fp_free(state->q_admitted_out);
+
+	fp_free(state->admitted_traffic_mempool);
+	fp_free(state->packet_mempool);
 }
 
 
 
 void emu_emulate(struct emu_state *state) {
-    uint32_t i;
-    
-    /* push/pull at endpoints and routers must be done in a specific order to
-     * ensure that packets pushed in one timeslot cannot be pulled until the
-     * next. */
+	uint32_t i;
 
-    /* push new packets from the network to endpoints */
-    for (i = 0; i < EMU_NUM_ENDPOINT_GROUPS; i++)
-        state->endpoint_drivers[i]->step();
-    
-    /* emulate one timeslot at each router (push and pull) */
-    for (i = 0; i < EMU_NUM_ROUTERS; i++)
-        state->router_drivers[i]->step();
-    
-    state->out->flush();
+	/* push/pull at endpoints and routers must be done in a specific order to
+	 * ensure that packets pushed in one timeslot cannot be pulled until the
+	 * next. */
+
+	/* push new packets from the network to endpoints */
+	for (i = 0; i < EMU_NUM_ENDPOINT_GROUPS; i++)
+		state->endpoint_drivers[i]->step();
+
+	/* emulate one timeslot at each router (push and pull) */
+	for (i = 0; i < EMU_NUM_ROUTERS; i++)
+		state->router_drivers[i]->step();
+
+	state->out->flush();
 }
 
 void emu_reset_sender(struct emu_state *state, uint16_t src) {
-    
-    /* TODO: clear the packets in the routers too? */
-    state->endpoint_groups[0]->reset(src);
+
+	/* TODO: clear the packets in the routers too? */
+	state->endpoint_groups[0]->reset(src);
 }
 
 /* frees all the packets in an fp_ring, and frees the ring itself */
 static inline void free_packet_ring(struct emu_state *state,
-                                    struct fp_ring *packet_ring)
+		struct fp_ring *packet_ring)
 {
-    struct emu_packet *packet;
-    
-    while (fp_ring_dequeue(packet_ring, (void **) &packet) == 0) {
-        free_packet(state, packet);
-    }
-    fp_free(packet_ring);
+	struct emu_packet *packet;
+
+	while (fp_ring_dequeue(packet_ring, (void **) &packet) == 0) {
+		free_packet(state, packet);
+	}
+	fp_free(packet_ring);
 }
 
 #ifdef NO_DPDK
 void emu_alloc_init(struct emu_state* state, uint32_t admitted_mempool_size,
-                    uint32_t admitted_ring_size, uint32_t packet_mempool_size,
-                    uint32_t packet_ring_size)
+		uint32_t admitted_ring_size, uint32_t packet_mempool_size,
+		uint32_t packet_ring_size)
 {
-    struct fp_mempool *admitted_traffic_mempool =
-        fp_mempool_create(admitted_mempool_size,
-                          sizeof(struct emu_admitted_traffic));
-    if (admitted_traffic_mempool == NULL)
-        throw std::runtime_error("couldn't allocate admitted_traffic_mempool");
-    
-    struct fp_ring *q_admitted_out = fp_ring_create("q_admitted_out",
-                                                    admitted_ring_size, 0, 0);
-    if (q_admitted_out == NULL)
-        throw std::runtime_error("couldn't allocate q_admitted_out");
+	struct fp_mempool *admitted_traffic_mempool =
+			fp_mempool_create(admitted_mempool_size,
+					sizeof(struct emu_admitted_traffic));
+	if (admitted_traffic_mempool == NULL)
+		throw std::runtime_error("couldn't allocate admitted_traffic_mempool");
 
-    struct fp_mempool *packet_mempool = fp_mempool_create(packet_mempool_size,
-                                                          EMU_ALIGN(sizeof(struct emu_packet)));
-    if (packet_mempool == NULL)
-        throw std::runtime_error("couldn't allocate packet_mempool");
+	struct fp_ring *q_admitted_out = fp_ring_create("q_admitted_out",
+			admitted_ring_size, 0, 0);
+	if (q_admitted_out == NULL)
+		throw std::runtime_error("couldn't allocate q_admitted_out");
 
-    struct fp_ring *q_new_packets = fp_ring_create("q_new_packets",
-                                                   packet_ring_size, 0, 0);
-    if (q_new_packets == NULL)
-        throw std::runtime_error("couldn't allocate q_new_packets");
+	struct fp_mempool *packet_mempool = fp_mempool_create(packet_mempool_size,
+			EMU_ALIGN(sizeof(struct emu_packet)));
+	if (packet_mempool == NULL)
+		throw std::runtime_error("couldn't allocate packet_mempool");
 
-    struct fp_ring *packet_queues[EMU_NUM_PACKET_QS];
-    packet_queues[1] = q_new_packets;
+	struct fp_ring *q_new_packets = fp_ring_create("q_new_packets",
+			packet_ring_size, 0, 0);
+	if (q_new_packets == NULL)
+		throw std::runtime_error("couldn't allocate q_new_packets");
 
-    emu_init_state(state, admitted_traffic_mempool, q_admitted_out,
-                   packet_mempool, packet_queues, R_DropTail, NULL,
-                   E_DropTail, NULL);
+	struct fp_ring *packet_queues[EMU_NUM_PACKET_QS];
+	packet_queues[1] = q_new_packets;
+
+	emu_init_state(state, admitted_traffic_mempool, q_admitted_out,
+			packet_mempool, packet_queues, R_DropTail, NULL,
+			E_DropTail, NULL);
 }
 #endif
--- emulation_test.cc
+++ emulation_test.cc
@@ -26,22 +26,17 @@ public:
           q_new_packets(fp_ring_create("", 1 << PACKET_Q_LOG_SIZE, 0, 0)),
           admitted_traffic_mempool(fp_mempool_create(ADMITTED_MEMPOOL_SIZE,
                                                      sizeof(struct emu_admitted_traffic))),
-          q_admitted_out(fp_ring_create("",1 << ADMITTED_Q_LOG_SIZE, 0, 0)),
-          m_emu_output(q_admitted_out, admitted_traffic_mempool, packet_mempool,
-                       &state.stat),
-          m_dropper(m_emu_output),
-          m_epg(EMU_NUM_ENDPOINTS, m_emu_output)
+          q_admitted_out(fp_ring_create("",1 << ADMITTED_Q_LOG_SIZE, 0, 0))
 	{
             uint16_t i;
-            uint32_t packet_size;
             struct drop_tail_args args;
             void *rtr_args;
 
-            args.q_capacity = 400; // about 4 ms at 1 Gbit/s
-        
+            /* initialize algo-specific state */
+            args.q_capacity = 400; // 4 milliseconds at 1 Gbit/s
+
             switch (rtype) {
             case R_DropTail:
-                m_router = new DropTailRouter(0, 5, m_dropper);
                 rtr_args = (void *) &args;
                 break;
                 
@@ -54,28 +49,22 @@ public:
                 red_params.max_p = 0.05;
                 red_params.wq_shift = 8;
 
-                m_router = new REDRouter(0, &red_params, m_dropper);
                 rtr_args = (void *) &red_params;
                 break;
             }
-                        
-            m_driver = new SingleRackNetworkDriver(q_new_packets, &m_epg, m_router, &state.stat, 1 << PACKET_Q_LOG_SIZE);
-                
-            packet_size = EMU_ALIGN(sizeof(struct emu_packet)) + 0;
-            m_epg.init(0, &args); // the endpoints are droptail
-            
+
+
             /* setup emulation state */
             struct fp_ring *packet_queues[EMU_NUM_PACKET_QS];
-            
+
             for (i = 0; i < EMU_NUM_PACKET_QS; i++) {
                 packet_queues[i] = fp_ring_create("", 1 << PACKET_Q_LOG_SIZE, 0, 0);
             }
             packet_queues[1] = q_new_packets;
-            
+
             emu_init_state(&state, admitted_traffic_mempool, q_admitted_out,
-                           packet_mempool, packet_queues, rtype, rtr_args,
+                           packet_mempool, packet_queues, rtype, &rtr_args,
                            E_DropTail, &args);
-            
 	}
 
     /**
@@ -83,72 +72,63 @@ public:
      */
     void emulate_and_print_admitted() {
         struct emu_admitted_traffic *admitted;
-        
+
         /* emulate one timeslot */
-        m_driver->step();
-        m_emu_output.flush();
-        
+        emu_emulate(&state);
+
         /* print out admitted traffic */
         while (fp_ring_dequeue(state.q_admitted_out, (void **) &admitted) != 0)
             printf("error: cannot dequeue admitted traffic\n");
-        
+
         admitted_print(admitted);
         fp_mempool_put(state.admitted_traffic_mempool, admitted);
     }
-    
+
+public:
+    struct emu_state state;
 private:
     struct fp_mempool *packet_mempool;
     struct fp_ring *q_new_packets;
     struct fp_mempool *admitted_traffic_mempool;
     struct fp_ring *q_admitted_out;
-public:
-    struct emu_state state;
-private:
-    EmulationOutput m_emu_output;
-    Dropper m_dropper;
-    DropTailEndpointGroup m_epg;
-    Router *m_router;
-    SingleRackNetworkDriver *m_driver;
 };
 
 int main() {
-	EmulationTest *test;
-	uint16_t i;
+    EmulationTest *test;
+    uint16_t i;
 
-	/* run a basic test of emulation framework */
-	test = new EmulationTest(R_DropTail);
-	printf("\nTEST 1: basic\n");
-	emu_add_backlog(&test->state, 0, 1, 0, 1); // src, dst, id, amount
-	emu_add_backlog(&test->state, 0, 3, 0, 3);
-	emu_add_backlog(&test->state, 7, 3, 0, 2);
+    /* run a basic test of emulation framework */
+    test = new EmulationTest(R_DropTail);
+    printf("\nTEST 1: basic\n");
+    emu_add_backlog(&test->state, 0, 1, 0, 1); // src, dst, id, amount
+    emu_add_backlog(&test->state, 0, 3, 0, 3);
+    emu_add_backlog(&test->state, 7, 3, 0, 2);
 
-	for (i = 0; i < 8; i++)
-		test->emulate_and_print_admitted();
-	delete test;
+    for (i = 0; i < 8; i++)
+        test->emulate_and_print_admitted();
+    delete test;
 
-	/* test drop-tail behavior at routers */
-	printf("\nTEST 2: drop-tail\n");
-	test = new EmulationTest(R_DropTail);
-	for (i = 0; i < 10; i++) {
-		emu_add_backlog(&test->state, i, 13, 0, 3);
-		test->emulate_and_print_admitted();
-	}
-	for (i = 0; i < 10; i++) {
-		test->emulate_and_print_admitted();
-	}
+    /* test drop-tail behavior at routers */
+    printf("\nTEST 2: drop-tail\n");
+    test = new EmulationTest(R_DropTail);
+    for (i = 0; i < 10; i++) {
+        emu_add_backlog(&test->state, i, 13, 0, 3);
+        test->emulate_and_print_admitted();
+    }
+    for (i = 0; i < 10; i++) {
+        test->emulate_and_print_admitted();
+    }
 
+    /* test RED behavior at routers */
+    printf("\nTEST 3: RED\n");
+    test = new EmulationTest(R_RED);
+    for (i = 0; i < 10; i++) {
+        emu_add_backlog(&test->state, i, 13, 0, 3);
+        test->emulate_and_print_admitted();
+    }
+    for (i = 0; i < 10; i++) {
+        test->emulate_and_print_admitted();
+    }
 
-	/* test RED behavior at routers */
-	printf("\nTEST 3: RED\n");
-	test = new EmulationTest(R_RED);
-	for (i = 0; i < 10; i++) {
-		emu_add_backlog(&test->state, i, 13, 0, 3);
-		test->emulate_and_print_admitted();
-	}
-	for (i = 0; i < 10; i++) {
-		test->emulate_and_print_admitted();
-	}
-
-
-	delete test;
+    delete test;
 }

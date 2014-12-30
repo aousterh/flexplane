a7d03147c423f40bc7acd2f2e8f2153a59b4667b
Amy Ousterhout <aousterh@mit.edu>
2014-12-30 17:07:30 -0500

    red/drop-tail now in queue_managers directory

--- src/arbiter/emu_admission_core.cc
+++ src/arbiter/emu_admission_core.cc
@@ -15,7 +15,7 @@
 #include "../emulation/admitted.h"
 #include "../emulation/emulation.h"
 #include "../emulation/endpoint.h"
-#include "../emulation/drop_tail.h"
+#include "../emulation/queue_managers/drop_tail.h"
 #include "../emulation/packet.h"
 #include "../emulation/router.h"
 #include "../graph-algo/algo_config.h"

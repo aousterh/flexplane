3addb9c36e26e7212be7b79bb46d3108b1dc95ed
Ambika Krishnamachar <ambikak@mit.edu>
2014-12-31 09:07:28 -0700

    Updated PDQ skeleton

--- python/pdq.py
+++ python/pdq.py
@@ -1,6 +1,114 @@
 #!/usr/bin/python
 
 from fastemu import *
+from pdq_helpers import *
+
+class PyTorRoutingTable(PyRoutingTable):
+    def route(self, pkt_p):
+        # need to convert from (struct emu_packet *) to (struct emu_packet)
+        pkt = pkt_value(pkt_p)
+        return pkt.dst
+
+class PyFlowIDClassifier(PyClassifier):
+    def classify(self, pkt_p, port):
+        # need to convert from (struct emu_packet *) to (struct emu_packet)
+        pkt = pkt_value(pkt_p)
+        return pkt.flow 
+
+"""
+PDQ Queue Manager
+Sets/ resets flow deadline in packet header based on queue length.
+
+Stores the same list of EndpointInfo objects as the Schedulers and Sink.
+
+Adds new flows to flow set & timer heap. 
+"""
+class PyPDQQueueManager(PyQueueManager):
+    # TODO: Calculate what to put here
+    SHORT_DEADLINE = 0
+    LONG_DEADLINE = 0
+    THRESHOLD = 0
+
+    def __init__(self, queue_bank, queue_capacity, dropper, endpoint_info_list):
+        super(PyDropQueueManager, self).__init__()
+        self.bank = queue_bank
+        self.capacity = queue_capacity
+        self.dropper = dropper
+        self.endpoint_info_list = endpoint_info_list
+
+    def enqueue(self, pkt_p, port, queue):
+        if self.bank.occupancy(port, queue) >= self.capacity:
+            self.dropper.drop(pkt_p)
+        else:
+            self.add_deadline_to_header(pkt_p, port, queue)
+
+            # Add new flow if necessary
+            if pkt_p.dest not in self.endpoint_info_list[pkt_p.src]:
+                self.add_new_flow_(pkt_p)
+
+            self.bank.enqueue(port, queue, pkt_p)
+
+    def add_deadline_to_header(self, pkt_p, port, queue):
+        if self.bank.occupancy(port, queue) < THRESHOLD:
+            deadline = SHORT_DEADLINE
+        else:
+            deadline = LONG_DEADLINE
+        ## TODO: Add deadline to packet header here. 
+
+    """
+    Adds info for a new flow to the PDQ layer flow set, as well 
+    as the timer heap. 
+    """
+    def add_new_flow(self, pkt_p):
+        pass
+
+"""
+Scheduler for the PDQ endpoints. Each "port" given to schedule() is a different endpoint.
+Each endpoint has n queues, one per destination.
+Schedule function determines which queue (if any) to send from
+in a given timeslot. 
+
+Scheduler stores a list of EndpointInfo objects, each of which contains
+relevant data structures for an endpoint:
+- PDQ_Layer
+- Timer heap
+- Timer
+(QM and Sink store a pointer to the same list of EndpointInfo objects.)
+
+Removes finished flows from timer heap. 
+"""
+class PyPDQEndpointScheduler(PyScheduler):
+    def __init__(self, queue_bank, n_queues_per_port, endpoint_info_list):
+        super(PyPDQEndpointScheduler, self).__init__()
+        self.bank = queue_bank
+        self.n_queues_per_port = n_queues_per_port
+        self.endpoint_info_list = endpoint_info_list
+
+    def schedule(self, port):
+        queue_id = self.select_queue(self, port) #select internally updates PDQ_layer
+        if queue_id == None:
+            return None
+
+        else:
+            return self.bank.dequeue(port, queue_id)
+            self.check_for_finished_flow(port, queue_id)
+
+    """
+    This was originally transmit() function.
+
+    Returns a queue to transmit from, or None if it is nobody's turn
+    to send. 
+    """
+    def select_queue(self, port):
+        pass
+
+    """
+    If the just-dequeued packet was the last in that queue, 
+    the flow has finished; removes it from timer heap and 
+    PDQ layer flow set. 
+    """
+    def check_for_finished_flow(self, port, queue_id):
+        pass
 
 """
 Scheduler for each PDQ router
@@ -9,101 +117,18 @@ Scheduler stores a set of flow state variables for each port.
 Updates packet header with flow state vars as packet is dequeued / exits router.
 """
 class PyPDQRouterScheduler(PyScheduler):
-    def __init__(self, queue_bank, num_ports):
-        super(PyPDQRouterScheduler, self).init()
+    def __init__(self, queue_bank, n_queues_per_port, endpoint_info_list):
+        super(PyPDQRouterScheduler, self).__init__()
         self.bank = queue_bank
-        self.PDQ_layers = []
-        for i in range(num_ports):
-            self.PDQ_layers.append(PDQ_Layer())
+        self.n_queues_per_port = 1
+        self.endpoint_info_list = endpoint_info_list
 
-    def schedule(self, port): # Assuming port arg is an index
+    def schedule(self, port):
         if not self.bank.empty(port, 0): # only 1 queue per port
             pkt =  self.bank.dequeue(port, 0) # Assuming dequeue func returns packet
             PDQ_layer = PDQ_layers[port]
             PDQ_layer.process_outgoing_packet(pkt, False)
-            return pkt
-
-"""
-Scheduler for the PDQ endpoints. Each "port" given to schedule func is a different endpoint.
-Each endpoint/port has n queues, one per destination.
-Schedule function determines which queue (if any) to send from
-in a given timeslot. 
-
-Scheduler stores PDQ layers, timer heaps, and timers for each endpoint.
-"""
-class PyPDQEndpointScheduler(PyScheduler):
-    def __init__(self, queue_bank, n_queues_per_port, flows=None):
-        super(PyPDQEndpointScheduler, self).init()
-        self.bank = queue_bank
-        self.n_queues_per_port = n_queues_per_port
-        self.endpoint_info_list = []
-        for i in range(n_queues_per_port):
-            timer_heap = None #### Change this -- Create timer heap with flows at that ep
-            endpoint_info = EndpointInfo(PDQ_Layer(), timer_heap, 0)
-            self.endpoint_info_list.append(endpoint_info)
-
-    def schedule(self, port):
-        ep_info = self.endpoint_info_list[port_index] #How do I get the port index? 
-        return self.transmit(ep_info)
-
-    def update_flows(self, flows): ### TODO: Replace "flows" with existing queue_bank
-        self.flows = flows
-        self.timer_heap = self.create_timer_heap()
-
-    def create_timer_heap(self):
-        timer_heap = []
-        for dest_id, flow in self.flows.iteritems():
-            heapq.heappush(timer_heap, (flow.time_to_send, flow))
-        self.timer_heap = timer_heap
-
-    """
-    Transmits to a particular destination based on whose "turn" it is.
-    Min-heap structure stores time-to-send, and when it is a flow's 
-    time to send, it sends, and resets its countdown. 
-
-    If a flow has missed its deadline, the flow is removed from the heap
-    and the next-in-line flow attempts to transmit. 
-    """
-    def transmit(self, ep_info):
-        if len(ep_info.timer_heap) == 0:
-            return None
-        if ep_info.timer_heap[0][0] <= ep_info.time:
-            if ep_info.timer_heap[0][1].deadline <= ep_info.time:
-                heapq.heappop(ep_info.timer_heap)
-                self.transmit(ep_info)
-            else:
-                # Reset the counter
-                (time_to_send, flow) = heapq.heappop(ep_info.timer_heap)
-                if flow.dest.id not in ep_info.PDQ_layer.flow_set:
-                    flow.R_s = 0
-                else:
-                    flow.R_s = ep_info.PDQ_layer.flow_set[flow.dest.id][0]
-
-                if flow.R_s == 0: #Create new probe
-                    probe_header = [flow.R_s, flow.R_max, flow.deadline, flow.expected_trans_time]
-                    pkt = PDQ_Packet(flow.src, flow.dest, False, probe_header, True, False, False)
-                    increment = 3
-                else:
-                    pkt = flow.flow_queue.pop(0)
-                    [R_h_old, R_max, D_h, T_h] = pkt.header
-                    pkt.header = [flow.R_s, R_max, D_h, T_h]
-                    increment = int(1/flow.R_s)
-
-                self.PDQ_layer.process_outgoing_packet(pkt, True)
-                heapq.heappush(ep_info.timer_heap, (time_to_send + increment, flow))
-                return pkt
-            return None
-
-"""
-Helper for the PDQ Endpoint Scheduler - stores info for each endpoint. 
-Time is an integer that keeps track of the current timeslot. 
-"""
-class EndpointInfo():
-    def __init__(PDQ_layer, timer_heap, time):
-        self.PDQ_Layer = PDQ_Layer
-        self.timer_heap = timer_heap
-        self.time = time
-
+            return pkt    
 
 """
 Handle function receives incoming packet - If packet is an ack, 
@@ -127,69 +152,3 @@ class PyPDQSink(PySink):
 
     def received_ack_update(self, ack):
         pass
-
-"""
-PDQ is a layer attached to every output queue in the router. 
-On packet exit from the router, PDQ recalculates flow info and
-adjusts the packet's header. 
-"""
-class PDQ_Layer():
-    def __init__(self):
-        self.flow_set = {} # Stores dict of src_id: [flow_info]
-
-    """
-    Upon packet exit, determines whether to update router's flow variables.
-    This function is called whenever a packet exits the router or endpoint.
-    'source' arg is True if processing at source; false otherwise.  
-    """
-    def process_outgoing_packet(self, packet, source):
-        if source == True: 
-            flow_index = packet.dest.id
-        else:
-            flow_index = packet.src.id
-        if packet.ack:
-            pass
-        elif packet.last: # Remove from flow set
-            del self.flow_set[flow_index]
-        else:
-            self.update_flow_states(packet, flow_index)
-            self.update_packet_header(packet, source, flow_index)
-    
-    def update_flow_states(self, packet, flow_index):
-        self.flow_set[flow_index] = packet.header
-
-        deadline_list = []
-        for flow_dest, flow_info in self.flow_set.iteritems():
-            deadline = flow_info[2]
-            deadline_list.append((deadline, flow_dest))
-
-        sorted_by_deadline = sorted(deadline_list)
-        remaining_bw = 1
-
-        # Give each flow some rate according to deadline
-        while (remaining_bw > 0 and len(sorted_by_deadline) > 0):
-            flow_dest = sorted_by_deadline.pop(0)[1]
-            max_rate_for_flow_dest = self.flow_set[flow_dest][1]
-            source_flow_rate = min(remaining_bw, max_rate_for_flow_dest) # Rate the source can give
-            switch_flow_rate = self.flow_set[flow_dest][0] #whatever the rate was before
-            
-            new_flow_rate = min(source_flow_rate, switch_flow_rate)
-            flow = self.flow_set[flow_dest]
-            flow[0] = new_flow_rate
-            self.flow_set[flow_dest] = flow
-            remaining_bw = remaining_bw - new_flow_rate
-
-        # Assign the rest of the flows rate 0
-        while len(sorted_by_deadline) > 0:
-            flow_dest = sorted_by_deadline.pop(0)[1]
-            self.flow_set[flow_dest][0] = 0
-
-    def update_packet_header(self, packet, source, flow_index):
-        packet_flow = self.flow_set[flow_index]
-        [R_h, R_max, D_h, T_h] = packet.header
-        if source == True:
-            packet.header = [1, R_max, D_h, T_h]
-        else:
-            packet.header = [packet_flow[0], R_max, D_h, T_h]
-
-

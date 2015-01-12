302eed129f162885c242e615de6b76247a95290a
Jonathan Perry <yonch@yonch.com>
2015-01-12 13:33:51 -0800

    fixed router factory

--- router.cc
+++ router.cc
@@ -8,18 +8,21 @@
 #include "router.h"
 #include "queue_managers/drop_tail.h"
 #include "queue_managers/red.h"
+#include "queue_managers/dctcp.h"
 #include <assert.h>
 #include "output.h"
 
 Router *RouterFactory::NewRouter(enum RouterType type, void *args, uint16_t id,
-		Dropper &dropper, struct queue_bank_stats *stats) {
+		Dropper &dropper, struct queue_bank_stats *stats)
+{
+  uint16_t q_capacity;
 	switch (type) {
 	case (R_DropTail):
-		uint16_t q_capacity =   (args == NULL)
-							  ? 128
-							  : ((struct drop_tail_args *)args)->q_capacity;
+          q_capacity =   (args == NULL)
+                       ? 128
+		       : ((struct drop_tail_args *)args)->q_capacity;
 		return new DropTailRouter(q_capacity, dropper, stats);
-            
+
 	case (R_RED):
 		assert(args != NULL);
 		return new REDRouter(id, (struct red_args *)args, dropper, stats);

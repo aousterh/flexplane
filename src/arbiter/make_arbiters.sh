4d31ed9751aef5f2ac2d0a07167407d92030f0bd
Amy Ousterhout <aousterh@mit.edu>
2015-01-21 13:20:59 -0800

    small changes to arbiter-building script

--- src/arbiter/make_arbiters.sh
+++ src/arbiter/make_arbiters.sh
@@ -3,13 +3,13 @@
 # compile one arbiter of each type for running experiments
 
 # make arbiter that runs RED
-make clean && make EMU_RTR_FLAGS=-DRED -j5
+make clean && make CONFIG_RTE_LIBRTE_PMD_PCAP=y EMU_RTR_FLAGS=-DRED -j22
 mv build/fast build/fast_red
 
 # make arbiter that runs DCTCP
-make clean && make EMU_RTR_FLAGS=-DDCTCP -j5
+make clean && make CONFIG_RTE_LIBRTE_PMD_PCAP=y EMU_RTR_FLAGS=-DDCTCP -j22
 mv build/fast build/fast_dctcp
 
 # make arbiter that runs drop tail
-make clean && make EMU_RTR_FLAGS=-DDROP_TAIL -j5
+make clean && make CONFIG_RTE_LIBRTE_PMD_PCAP=y EMU_RTR_FLAGS=-DDROP_TAIL -j22
 cp build/fast build/fast_drop_tail

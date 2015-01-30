4e373e757431f1faaa2b4148529d3cad8e1087c7
Amy Ousterhout <aousterh@mit.edu>
2015-01-29 22:21:56 -0500

    use masks instead of mod in core routing table

--- routing_tables/CoreRoutingTable.h
+++ routing_tables/CoreRoutingTable.h
@@ -19,15 +19,13 @@ class CoreRoutingTable : public RoutingTable {
 public:
 	/**
 	 * c'tor
-	 * @param rack_shift: the number of bits to shift an endpoint ID to get
-	 *      the index of each rack. The lower bits are the endpoint within the
-	 *      rack.
-	 * @param links_per_tor: number of links to each tor switch
+	 * @param tor_mask: mask for number of links per tor. we assume this is the
+	 * 	negation of the mask to get the rack id
 	 *
 	 * @assumes tors are connected to ports tor*links_per_tor...
 	 * 	(tor+1)*links_per_tor-1
 	 */
-	CoreRoutingTable(uint32_t rack_shift, uint32_t links_per_tor);
+	CoreRoutingTable(uint32_t tor_mask);
 
 	/**
 	 * d'tor
@@ -37,28 +35,21 @@ public:
 	inline uint32_t route(struct emu_packet *pkt);
 
 private:
-	/** number of bits to shift endpoint IDs to get the rack */
-	uint32_t m_rack_shift;
-
-	/** links per tor router */
-	uint32_t m_links_per_tor;
+	/** mask for links to tor */
+	uint32_t m_tor_mask;
 };
 
-inline CoreRoutingTable::CoreRoutingTable(uint32_t rack_shift,
-		uint32_t links_per_tor)
-	: m_rack_shift(rack_shift),
-	  m_links_per_tor(links_per_tor)
+inline CoreRoutingTable::CoreRoutingTable(uint32_t tor_mask)
+	: m_tor_mask(tor_mask)
 {}
 
 inline CoreRoutingTable::~CoreRoutingTable() {}
 
 inline uint32_t CoreRoutingTable::route(struct emu_packet *pkt)
 {
-	uint16_t rack_id = (pkt->dst >> m_rack_shift);
-
 	/* use a hash to choose between the links to the correct tor */
 	uint32_t hash =  7 * pkt->src + 9 * pkt->dst + pkt->flow;
-	return (hash % m_links_per_tor) + rack_id * m_links_per_tor;
+	return (hash & m_tor_mask) + (pkt->dst & ~m_tor_mask);
 }
 
 #endif /* ROUTINGTABLES_CORE_H_ */

4b568b64300e3099342ff016c79d866ddaafb426
Jonathan Perry <yonch@yonch.com>
2015-01-29 22:26:46 -0500

    added new arbiter types to make_arbiters.sh

--- src/arbiter/make_arbiters.sh
+++ src/arbiter/make_arbiters.sh
@@ -13,3 +13,16 @@ mv build/fast build/fast_dctcp
 # make arbiter that runs drop tail
 make clean && make CONFIG_RTE_LIBRTE_PMD_PCAP=y EMU_RTR_FLAGS=-DDROP_TAIL -j22
 cp build/fast build/fast_drop_tail
+
+# make arbiter that runs priority queueing
+make clean && make CONFIG_RTE_LIBRTE_PMD_PCAP=y EMU_RTR_FLAGS=-DPRIORITY_QUEUEING -j22
+cp build/fast build/fast_prio
+
+# make arbiter that runs round robin
+make clean && make CONFIG_RTE_LIBRTE_PMD_PCAP=y EMU_RTR_FLAGS=-DROUND_ROBIN -j22
+cp build/fast build/fast_rr
+
+# make arbiter that runs HULL
+make clean && make CONFIG_RTE_LIBRTE_PMD_PCAP=y EMU_RTR_FLAGS=-DHULL -j22
+cp build/fast build/fast_hull
+

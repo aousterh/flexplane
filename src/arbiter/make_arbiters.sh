ab7d6012c354895a6c95e4203cad792e9c7cea3a
Amy Ousterhout <aousterh@mit.edu>
2015-04-03 08:42:05 -0700

    fix make_arbiters script so that drop tail is still default

--- src/arbiter/make_arbiters.sh
+++ src/arbiter/make_arbiters.sh
@@ -10,18 +10,18 @@ mv build/fast build/fast_red
 make clean && make CONFIG_RTE_LIBRTE_PMD_PCAP=y EMU_RTR_FLAGS=-DDCTCP -j22
 mv build/fast build/fast_dctcp
 
-# make arbiter that runs drop tail
-make clean && make CONFIG_RTE_LIBRTE_PMD_PCAP=y EMU_RTR_FLAGS=-DDROP_TAIL -j22
-cp build/fast build/fast_drop_tail
-
 # make arbiter that runs priority queueing
 make clean && make CONFIG_RTE_LIBRTE_PMD_PCAP=y EMU_RTR_FLAGS=-DPRIO_QUEUEING -j22
-cp build/fast build/fast_prio
+mv build/fast build/fast_prio
 
 # make arbiter that runs round robin
 make clean && make CONFIG_RTE_LIBRTE_PMD_PCAP=y EMU_RTR_FLAGS=-DROUND_ROBIN -j22
-cp build/fast build/fast_rr
+mv build/fast build/fast_rr
 
 # make arbiter that runs HULL
 make clean && make CONFIG_RTE_LIBRTE_PMD_PCAP=y EMU_RTR_FLAGS=-DHULL -j22
-cp build/fast build/fast_hull
+mv build/fast build/fast_hull
+
+# make arbiter that runs drop tail. drop tail is the default and must come last.
+make clean && make CONFIG_RTE_LIBRTE_PMD_PCAP=y EMU_RTR_FLAGS=-DDROP_TAIL -j22
+cp build/fast build/fast_drop_tail

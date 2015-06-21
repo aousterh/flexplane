212740526859fc8d2a1587e38e163d369e71ec7b
Amy Ousterhout <aousterh@mit.edu>
2015-06-21 14:12:53 -0400

    python rpc client sends at a target qps

--- src/rpc_tool/client.py
+++ src/rpc_tool/client.py
@@ -7,7 +7,6 @@ from stats import *
 
 # max possible to fit in one 1500 byte MTU
 response_size = 1448
-
 receive_buffer_size = 1024 * 1024
 
 def setup_socket(server_addr, server_port):
@@ -17,13 +16,22 @@ def setup_socket(server_addr, server_port):
 
     return client_socket
 
-def run_client(server_addr, server_port):
+def run_client(server_addr, server_port, qps):
     avail_socket = setup_socket(server_addr, server_port)
     stats = ConnectionStats(time.time())
 
     print ConnectionStats.header_names()
 
+    t_btwn_requests = 1.0 / qps
+    current_time = time.time()
+    next_send_time = current_time + t_btwn_requests
+
     while 1:
+        # spin until next send time
+        while current_time < next_send_time:
+            current_time = time.time()
+        # update next sent time
+        next_send_time += t_btwn_requests
 
         current_socket = avail_socket
 
@@ -47,13 +55,14 @@ def run_client(server_addr, server_port):
 
 def main():
     args = sys.argv
-    if len(args) < 3:
-        print "Usage: python %s server_addr server_port" % str(args[0])
+    if len(args) < 4:
+        print "Usage: python %s server_addr server_port qps" % str(args[0])
         exit()
     server_addr = args[1]
     server_port = int(args[2])
+    qps = int(args[3])
 
-    run_client(server_addr, server_port)
+    run_client(server_addr, server_port, qps)
 
 if __name__ == "__main__":
     main()

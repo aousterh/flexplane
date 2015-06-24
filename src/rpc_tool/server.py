90bbc28ba8b05f44df7359414ea5c374660dcb4c
Amy Ousterhout <aousterh@mit.edu>
2015-06-24 12:21:02 -0400

    generate large response before connecting on server side

--- src/rpc_tool/server.py
+++ src/rpc_tool/server.py
@@ -10,6 +10,11 @@ def generate_response(size):
     response_dict[size] = data
 
 def run_server(params):
+    # prepare a response before connecting if a size is specified. this is
+    # helpful when responses are large and take a long time to generate
+    if params.r_size is not None:
+        generate_response(params.r_size)
+
     # set up a socket
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serversocket.bind((params.server_addr, params.server_port))
@@ -37,6 +42,8 @@ def get_args():
                         help='the IP address or hostname of this server')
     parser.add_argument('server_port', metavar='s_port', type=int,
                         help='the port of this server')
+    parser.add_argument('--r_size', metavar='r_size', type=int, required=False,
+                        help='the size of the response (1448 is max for 1 MTU)')
 
     return parser.parse_args()
 

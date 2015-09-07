4f384900071e5ae3cfee211f931f490fed8acb6f
Amy Ousterhout <aousterh@mit.edu>
2015-09-07 14:50:33 -0400

    add a test for pFabric routers

---
+++ tests/pfabric_unittest.cc
@@ -0,0 +1,61 @@
+/*
+ * pfabric_unittest.cc
+ *
+ *  Created on: September 7, 2015
+ *      Author: aousterh
+ */
+
+#include "emulation.h"
+#include "emulation_container.h"
+#include "emu_topology.h"
+#include "gtest/gtest.h"
+
+/*
+ * Test that packets from two different flows are dequeued in the right order.
+ */
+TEST(PFabricTest, two_flows) {
+	struct emu_topo_config topo_config;
+	EmulationContainer *container;
+	u8 areq_data_0[4*4] = {0, 0, 0, 10, 0, 0, 0, 9, 0, 0, 0, 8, 0, 0, 0, 7};
+	u8 areq_data_1[4*3] = {0, 0, 0, 11, 0, 0, 0, 6, 0, 0, 0, 5};
+	struct emu_admitted_traffic *admitted;
+	uint16_t i;
+	uint16_t expected_ids[7] = {20, 10, 11, 12, 21, 22, 23};
+
+
+	/* initialize emulated topology */
+	topo_config.num_racks = 1;
+	topo_config.rack_shift = 5; /* 32 machines per rack */
+
+	/* initialize emulation container */
+	container = new EmulationContainer(ADMITTED_MEMPOOL_SIZE,
+			(1 << ADMITTED_Q_LOG_SIZE), PACKET_MEMPOOL_SIZE,
+			(1 << PACKET_Q_LOG_SIZE), R_PFabric, NULL, E_Simple, NULL,
+			&topo_config);
+
+    /* src, dst, flow, amount, start_id, pointer to additional data */
+	container->add_backlog(12, 14, 0, 4, 20, &areq_data_0[0]);
+	container->add_backlog(13, 14, 0, 3, 10, &areq_data_1[0]);
+
+	/* first 3 admitted structs should be empty */
+	for (i = 0; i < 3; i++) {
+		container->step();
+
+		/* check that admitted is empty */
+		admitted = container->get_admitted();
+		EXPECT_EQ(0, admitted->size);
+		container->free_admitted(admitted);
+	}
+
+	for (i = 0; i < 7; i++) {
+		container->step();
+
+		/* check admitted */
+		admitted = container->get_admitted();
+		EXPECT_EQ(1, admitted->size);
+		EXPECT_EQ(expected_ids[i], admitted->edges[0].id);
+		container->free_admitted(admitted);
+	}
+
+	delete container;
+}

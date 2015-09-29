84ccfc16283ff89968f2f92123d63b52d61e978f
Amy Ousterhout <aousterh@mit.edu>
2015-09-29 18:33:53 -0400

    fix bug in TSO

--- schedulers/SingleQueueTSOScheduler.h
+++ schedulers/SingleQueueTSOScheduler.h
@@ -23,21 +23,31 @@ public:
 		: m_bank(bank) {
 		uint16_t i;
 
-		for (i = 0; i < n_ports; i++)
+		for (i = 0; i < n_ports; i++) {
 			m_wait_time.push_back(0);
+			m_last_update_time.push_back(0);
+		}
 	}
 
 	inline struct emu_packet *schedule(uint32_t output_port, uint64_t cur_time,
 			Dropper *dropper)
 	{
+		uint64_t wait_time_decrease;
+		struct emu_packet *p;
+
 		if (unlikely(m_bank->empty(output_port, 0)))
 			throw std::runtime_error("called schedule on an empty port");
 
-		struct emu_packet *p;
+		/* update wait time, deducting one for each timeslot that has passed
+		 * since this function was last called */
+		wait_time_decrease = cur_time - m_last_update_time[output_port];
+		if (wait_time_decrease >= m_wait_time[output_port])
+			m_wait_time[output_port] = 0;
+		else
+			m_wait_time[output_port] -= wait_time_decrease;
+		m_last_update_time[output_port] = cur_time;
 
-		if (m_wait_time[output_port] > 0)
-			m_wait_time[output_port]--;
-
+		/* check if we can send now or not */
 		if (m_wait_time[output_port] > 0) {
 			return NULL; /* can't send yet */
 		}
@@ -59,6 +69,9 @@ protected:
 
 	/** number of timeslots until next send time, per port */
 	std::vector<uint32_t> m_wait_time;
+
+	/** last time we updated the wait time, per port */
+	std::vector<uint32_t> m_last_update_time;
 };
 
 #endif /* SCHEDULERS_SINGLEQUEUETSOSCHEDULER_H_ */

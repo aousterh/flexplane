bbf75e3004d72cce3196882e7c2140f0aef8fed5
Amy Ousterhout <aousterh@mit.edu>
2015-09-29 18:28:01 -0400

    add tests for drop tail with TSO

--- tests/drop_tail_tso_unittest.cc
+++ tests/drop_tail_tso_unittest.cc
@@ -1,29 +1,28 @@
 /*
- * round_robin_unittest.cc
+ * drop_tail_tso_unittest.cc
  *
- *  Created on: September 23, 2015
+ *  Created on: September 28, 2015
  *      Author: aousterh
  */
 
 #include "emulation.h"
 #include "emulation_container.h"
 #include "emu_topology.h"
-#include "queue_managers/drop_tail.h"
-#include "schedulers/RRScheduler.h"
+#include "queue_managers/drop_tail_tso.h"
 #include "gtest/gtest.h"
+#include "../protocol/flags.h"
 
 /*
- * Test that packets from different flows are dequeued in the right order.
+ * Test that packets from the same flow are dequeued at the correct time when
+ * 	there is a continuous backlog of packets to send.
  */
-TEST(RoundRobinTest, two_flows) {
+TEST(DropTailTSOTest, one_flow_backlogged) {
 	struct emu_topo_config topo_config;
 	struct drop_tail_args rtr_args;
 	EmulationContainer *container;
 	struct emu_admitted_traffic *admitted;
 	uint16_t i;
-	uint16_t expected_ids_0[9] = {20, 10, 21, 11, 22, 12, 23, 13, 24};
-	uint16_t expected_ids_1[9] = {10, 20, 11, 21, 12, 22, 13, 23, 24};
-	uint16_t *expected_ids;
+	u8 areq_data[2 * 3] = { 4, 0, 1, 0, 3, 0 };
 
 	/* initialize emulated topology */
 	topo_config.num_racks = 1;
@@ -35,12 +34,11 @@ TEST(RoundRobinTest, two_flows) {
 	/* initialize emulation container */
 	container = new EmulationContainer(ADMITTED_MEMPOOL_SIZE,
 			(1 << ADMITTED_Q_LOG_SIZE), PACKET_MEMPOOL_SIZE,
-			(1 << PACKET_Q_LOG_SIZE), R_RR, &rtr_args, E_Simple, NULL,
-			&topo_config);
+			(1 << PACKET_Q_LOG_SIZE), R_DropTailTSO, &rtr_args, E_SimpleTSO,
+			NULL, &topo_config);
 
 	/* src, dst, flow, amount, start_id, pointer to additional data */
-	container->add_backlog(12, 14, 0, 5, 20, NULL);
-	container->add_backlog(13, 14, 0, 4, 10, NULL);
+	container->add_backlog(12, 14, 0, 3, 20, &areq_data[0]);
 
 	/* first 3 admitted structs should be empty */
 	for (i = 0; i < 3; i++) {
@@ -57,15 +55,13 @@ TEST(RoundRobinTest, two_flows) {
 
 		/* check admitted */
 		admitted = container->get_admitted();
-		EXPECT_EQ(1, admitted->size);
 
 		/* the choice of which goes first is random */
-		if (i == 0 && admitted->edges[0].src == 12)
-			expected_ids = &expected_ids_0[0];
-		else if (i == 0)
-			expected_ids = &expected_ids_1[0];
-
-		EXPECT_EQ(expected_ids[i], admitted->edges[0].id);
+		if ((i == 0) || (i == 4) || (i == 5)) {
+			EXPECT_EQ(1, admitted->size);
+		}
+		else
+			EXPECT_EQ(0, admitted->size);
 
 		container->free_admitted(admitted);
 	}
@@ -73,20 +69,17 @@ TEST(RoundRobinTest, two_flows) {
 	delete container;
 }
 
-
 /*
- * Test that packets are dequeued in the right order with multiple ports active
- * at the same time.
+ * Test that packets from the same flow are dequeued at the correct time when
+ * 	the flow is on and then off and then on again.
  */
-TEST(RoundRobinTest, two_ports) {
+TEST(DropTailTSOTest, one_flow_on_off) {
 	struct emu_topo_config topo_config;
 	struct drop_tail_args rtr_args;
 	EmulationContainer *container;
 	struct emu_admitted_traffic *admitted;
 	uint16_t i;
-	uint16_t expected_ids_0[9] = {20, 10, 21, 11, 22, 12, 23, 13, 24};
-	uint16_t expected_ids_1[9] = {10, 20, 11, 21, 12, 22, 13, 23, 24};
-	uint16_t *expected_ids;
+	u8 areq_data[2 * 3] = { 4, 0, 2, 0, 3, 0 };
 
 	/* initialize emulated topology */
 	topo_config.num_racks = 1;
@@ -98,13 +91,11 @@ TEST(RoundRobinTest, two_ports) {
 	/* initialize emulation container */
 	container = new EmulationContainer(ADMITTED_MEMPOOL_SIZE,
 			(1 << ADMITTED_Q_LOG_SIZE), PACKET_MEMPOOL_SIZE,
-			(1 << PACKET_Q_LOG_SIZE), R_RR, &rtr_args, E_Simple, NULL,
-			&topo_config);
+			(1 << PACKET_Q_LOG_SIZE), R_DropTailTSO, &rtr_args, E_SimpleTSO,
+			NULL, &topo_config);
 
 	/* src, dst, flow, amount, start_id, pointer to additional data */
-	container->add_backlog(12, 14, 0, 5, 20, NULL);
-	container->add_backlog(13, 14, 0, 4, 10, NULL);
-	container->add_backlog(15, 16, 0, 5, 0, NULL);
+	container->add_backlog(12, 14, 0, 2, 20, &areq_data[0]);
 
 	/* first 3 admitted structs should be empty */
 	for (i = 0; i < 3; i++) {
@@ -116,26 +107,40 @@ TEST(RoundRobinTest, two_ports) {
 		container->free_admitted(admitted);
 	}
 
-	for (i = 0; i < 5; i++) {
+	for (i = 0; i < 6; i++) {
 		container->step();
 
 		/* check admitted */
 		admitted = container->get_admitted();
-		EXPECT_EQ(2, admitted->size);
 
 		/* the choice of which goes first is random */
-		if (i == 0 && admitted->edges[0].src == 12)
-			expected_ids = &expected_ids_0[0];
-		else if (i == 0)
-			expected_ids = &expected_ids_1[0];
-
-		/* ports are processed in order, so edge from dst 14 should appear
-		 * first */
-		EXPECT_EQ(expected_ids[i], admitted->edges[0].id);
-		EXPECT_EQ(i, admitted->edges[1].id);
+		if ((i == 0) || (i == 4)) {
+			EXPECT_EQ(1, admitted->size);
+		}
+		else
+			EXPECT_EQ(0, admitted->size);
 
 		container->free_admitted(admitted);
 	}
+
+	/* add more backlog again */
+	container->add_backlog(12, 14, 0, 1, 22, &areq_data[2]);
+
+	/* next 3 should be empty again */
+	for (i = 0; i < 3; i++) {
+		container->step();
+
+		/* check that admitted is empty */
+		admitted = container->get_admitted();
+		EXPECT_EQ(0, admitted->size);
+		container->free_admitted(admitted);
+	}
+
+	/* should be admitted immediately */
+	container->step();
+	admitted = container->get_admitted();
+	EXPECT_EQ(1, admitted->size);
+	container->free_admitted(admitted);
 
 	delete container;
 }

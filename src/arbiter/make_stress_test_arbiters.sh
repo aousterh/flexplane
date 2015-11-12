6470c29a31a0b26cbede5ee599d0b1954daafa94
Amy Ousterhout <aousterh@mit.edu>
2015-11-11 21:25:42 -0500

    experiment-running scripts support multiple iterations

--- src/arbiter/make_stress_test_arbiters.sh
+++ src/arbiter/make_stress_test_arbiters.sh
@@ -3,26 +3,33 @@
 # build several emulators with different numbers of racks, all with 1 Core switch
 # first arg is "mlx" if we should compile for mellanox, empty otherwise
 
-n_racks=( 2 3 4 5 6 7 8 )
-n_comms=2
+n_racks=( 1 2 3 4 5 6 7 8 )
 
 for racks in "${n_racks[@]}"
 do
-    cores=$((racks + 1))
-#    cores=$((racks))
-    echo "Building emulator with $racks racks and $cores cores"
+    # set number of cores and number of comms
+#    cores=$((racks + 1))
+    cores=$((racks))
+    if [ $racks = 1 ]; then
+        comms=1
+    else
+        comms=2
+    fi
 
+    echo "Building emulator with $racks racks, $cores cores, and $comms comms"
+    
+    
     if [ "$#" -ne 1 ]; then
         # build for intel
-        make clean && make CMD_LINE_CFLAGS+=-DALGO_N_CORES=$cores CMD_LINE_CFLAGS+=-DEMU_NUM_RACKS=$racks -j22
+        make clean && make CMD_LINE_CFLAGS+=-DALGO_N_CORES=$cores CMD_LINE_CFLAGS+=-DEMU_NUM_RACKS=$racks CMD_LINE_CFLAGS+=-DN_COMM_CORES=$comms -j22
     elif [ "$1" = "mlx" ]; then
         # build for mellanox
-        make clean && make CONFIG_RTE_LIBRTE_PMD_PCAP=y CMD_LINE_CFLAGS+=-DALGO_N_CORES=$cores CMD_LINE_CFLAGS+=-DEMU_NUM_RACKS=$racks -j22
+        make clean && make CONFIG_RTE_LIBRTE_PMD_PCAP=y CMD_LINE_CFLAGS+=-DALGO_N_CORES=$cores CMD_LINE_CFLAGS+=-DEMU_NUM_RACKS=$racks CMD_LINE_CFLAGS+=-DN_COMM_CORES=$comms -j22
     else
         echo "unrecognized argument $1"
     fi
 
-    cp build/fast build/fast_"$racks"_racks_"$n_comms"_comms
+    cp build/fast build/fast_"$racks"_racks_"$comms"_comms
 done
 
 # copy build arbiters to build directory for deployment to arbiter machines
--- src/arbiter/run_different_topos.sh
+++ src/arbiter/run_different_topos.sh
@@ -3,8 +3,9 @@
 # run several emulators with different numbers of racks, all with 1 Core switch
 # first arg is "mlx" if we should compile for mellanox, empty otherwise
 
-n_racks=( 2 3 4 5 6 7 8 )
-n_comms=2
+n_racks=( 1 2 3 4 5 6 7 8 )
+n_iterations=5
+description="multiple_admitted_mempools"
 
 # create a directory for logging
 mkdir /tmp/aousterh-logs
@@ -12,27 +13,43 @@ mkdir /tmp/aousterh-logs
 # shield cpus appropriately
 sudo cset shield -c 1-14
 
-for racks in "${n_racks[@]}"
+
+OUT_SUMMARY="emu_output_summary.csv"
+echo "racks,comms,index,tput,label" > $OUT_SUMMARY
+
+for i in `seq 1 $n_iterations`;
 do
-    OUT_FILE="emu_output_${racks}_racks_${n_comms}_comms.out"
-    ERR_FILE="emu_output_${racks}_racks_${n_comms}_comms.err"
-    echo "running topo with $racks racks, outfile: $OUT_FILE"
+    for racks in "${n_racks[@]}"
+    do
+        if [ $racks = 1 ]; then
+            comms=1
+        else
+            comms=2
+        fi
+        
+        OUT_FILE="emu_output_${racks}_racks_${comms}_comms_${i}.out"
+        ERR_FILE="emu_output_${racks}_racks_${comms}_comms_${i}.err"
+        echo "running topo with $racks racks, outfile: $OUT_FILE"
 
-    FAST=build/fast_"$racks"_racks_"$n_comms"_comms
+        FAST=build/fast_"$racks"_racks_"$comms"_comms
 
-    if [ "$#" -ne 1 ]; then
-        sudo cset shield -e -- $FAST -c 7ffe -n 3 --no-hpet -- -p 1 > $OUT_FILE 2> $ERR_FILE
-    elif [ "$1" = "mlx" ]; then
-        # run on mellanox
-        sudo cset shield -e -- $FAST -c 7ffe -n 3 --no-hpet -d ./librte_pmd_mlx4.so -- -p 1 > $OUT_FILE 2> $ERR_FILE
-    else
-        echo "unrecognized argument $1"
-    fi
+        if [ "$#" -ne 1 ]; then
+            sudo cset shield -e -- $FAST -c 7ffe -n 3 --no-hpet -- -p 1 > $OUT_FILE 2> $ERR_FILE
+        elif [ "$1" = "mlx" ]; then
+            # run on mellanox
+            sudo cset shield -e -- $FAST -c 7ffe -n 3 --no-hpet -d ./librte_pmd_mlx4.so -- -p 1 > $OUT_FILE 2> $ERR_FILE
+        else
+            echo "unrecognized argument $1"
+        fi
 
-    grep "30 seconds" $OUT_FILE
+        grep "30 seconds" $OUT_FILE
 
-    # pause to allow huge pages to become available again
-    sleep 1s
+        TPUT=`grep "30 seconds" $OUT_FILE | cut -f 6 -d ' ' | awk '{ SUM += $1} END { print SUM }'`
+        echo "${racks},${comms},${i},${TPUT},${description}" >> $OUT_SUMMARY
+        
+        # pause to allow huge pages to become available again
+        sleep 1s
+    done
 done
 
 sudo cset shield --reset
